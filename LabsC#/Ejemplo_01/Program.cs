a2b4bbe4e61d92844fa3af915f52627cdd23f178
JafetProg <160031621+JafetProg@users.noreply.github.com>
2025-04-23 13:07:01 -0400

    Add files via upload

---
+++ LabsC#/Ejemplo_01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program 
+{
+    static void Main() 
+    {
+        int myVariable = 10;
+        const int myConstant = 20;
+        Console.WriteLine("Valor de mi variable: " + myVariable);
+        Console.WriteLine("Valor de mi constante: " + myConstant);
+        Console.ReadKey();
+    }
+}
