e461c9b377b37f917a31ef2f9576f0674f44a2d3
JafetProg <160031621+JafetProg@users.noreply.github.com>
2025-04-23 13:01:33 -0400

    Add files via upload

---
+++ Labs_C++/Ejemplo_02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()  
+    {
+        string saludo = "Hola, Mundo!";
+        cout << saludo << endl;
+        return 0;
+    }
