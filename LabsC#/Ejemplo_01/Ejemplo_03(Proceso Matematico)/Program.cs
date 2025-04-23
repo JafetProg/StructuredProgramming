e6a0272d51d0a222f22be38d0ab440cda75e787c
JafetProg <160031621+JafetProg@users.noreply.github.com>
2025-04-23 13:10:05 -0400

    Add files via upload

---
+++ LabsC#/Ejemplo_01/Ejemplo_03(Proceso Matematico)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+        double num1,num2,media;
+
+        Console.Write("Introduzca el primer numero: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Introduzca el segundo numero:");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        media = (num1+num2)/2;
+        
+        Console.WriteLine("La media es:"+ media);
+    }
+}
